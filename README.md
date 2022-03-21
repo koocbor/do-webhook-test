5bdd6d54fcdfcf3b7d7b104b91d27f5d5887267d
Rob Cook <robcook@gmail.com>
2022-03-21 19:17:18 -0400

    first attempt with button

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Webhook Test
 
 A sample webhook that stores the body posted to a simple mongodb instance.
+
+[![Deploy to DO](https://www.deploytodo.com/do-btn-blue.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/koocbor/do-webhook-test/tree/main)
--- src/app.js
+++ src/app.js
@@ -1,62 +1,46 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const { v4: uuidv4 } = require('uuid');
 
-const url = 'mongodb://root:example@mongo-db:27017';
-const client = new MongoClient(url);
-
+// Use environment var or create a UUID to act as a "key" to prevent
+// unauthorized POST / GET requests.
 const secret = process.env.API_KEY || uuidv4();
 
 const app = express();
 app.use(express.json())
 const port = process.env.PORT || 3000
 
-async function run() {
-    await client.connect();
-    const collection = client.db('webhook-test').collection('timeseries');
+const values = [];
 
-    app.get('/health', async (req, res) => {
-        await client.db().command({ ping: 1 });
-        res.send();
-    })
-    
-    app.get('/', async (req, res) => {
-        console.log(req.headers);
-        const reqSecret = req.header('x-api-key');
-        console.log(reqSecret);
-        if (reqSecret !== secret) {
-            res.sendStatus(401);
-            return;
-        }
 
-        // sort by descending timestamp
-        const query = {};
-        const sort = '{ ts: -1 }';
-        const limit = 10;
-    
-        const cursor = await collection.find(query).sort(sort).limit(limit);
-        const body = await cursor.toArray();
-        res.send(body);
-    })
-    
-    app.post('/', async (req, res) => {
-        const reqSecret = req.header('x-api-secret');
-        if (reqSecret !== secret) {
-            res.sendStatus(401);
-        }
+app.get('/health', async (req, res) => {
+    res.send();
+})
 
-        const doc = req.body;
-        
-        console.log(doc);
-    
-        await collection.insertOne(doc);
-    
-        res.sendStatus(200);
-    })
+app.get('/', async (req, res) => {
+    const reqSecret = req.header('x-api-key');
+    if (reqSecret !== secret) {
+        res.sendStatus(401);
+        return;
+    }
+    res.send(values);
+})
 
-    app.listen(port, () => {
-        console.log(`webhook listening on port ${port}, api-secret: ${secret}`);
-    })
-}
-run();
+app.post('/', async (req, res) => {
+    const reqSecret = req.header('x-api-key');
+    if (reqSecret !== secret) {
+        res.sendStatus(401);
+    }
 
+    const doc = req.body;
+    values.unshift(doc);
+
+    if (values.length > 10) {
+        values.length = 10;
+    }
+
+    res.sendStatus(200);
+})
+
+app.listen(port, () => {
+    console.log(`webhook listening on port ${port}, api-secret: ${secret}`);
+})
